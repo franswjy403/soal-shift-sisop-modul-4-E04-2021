b4eec84ab5af955828c9336afc8afffd9a98bc72
Frans <franswijaya403@gmail.com>
2021-06-03 11:21:10 +0700

    First Commit

---
+++ SinSeiFS_E04.c
@@ -0,0 +1,21 @@
+#define FUSE_USE_VERSION 28
+#include<fuse.h>
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<dirent.h>
+#include<errno.h>
+#include<sys/time.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdbool.h>
+
+char dirpath[50] = "/home/frans/Downloads";
+
+
+
+int main(){
+    
+}
